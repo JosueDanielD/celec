9bdbbcf605c21298d584b330a2d00e2c34b0a46d
Josué Daniel Díaz Baque <josdbaqu@espol.edu.ec>
2025-07-30 11:04:02 -0500

    Taller03Terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+
+
 
 import Grid from '@mui/material/Grid2' 
 import HydrologyTable from './components/HydrologyTable'
@@ -9,16 +12,28 @@ import DataHour from './interface/DataHour'
 
 function App() {
 
-  let url = "https://raw.githubusercontent.com/aavendan/datos/refs/heads/main/CELEC/hidrologia_17122024.json"
+  const url = "https://raw.githubusercontent.com/aavendan/datos/refs/heads/main/CELEC/hidrologia_17122024.json"
   
   // PENDIENTE: Variable de estado y la función de modificación. 
-
+  const [data, setData] = useState<DataHour[]>([]);
 
   // PENDIENTE: 
   // Realizar una petición asíncrona a la URL. La respuesta es un JSON. 
   // Al recibir la respuesta, actualice la variable de estado.
 
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
 
   return (
@@ -28,7 +43,11 @@ function App() {
         <Grid size={{ xs: 12 }}>
 
           {/* PENDIENTE: Envíe sus datos (apellidos, nombres y paralelo) como props del componente */}
-          <Student></Student>
+          <Student
+            apellidos="Díaz"
+            nombres="Josué"
+            paralelo="101"
+          />
 
         </Grid>
         
@@ -36,7 +55,7 @@ function App() {
         <Grid size={{ xs: 12 }}>
 
           {/* PENDIENTE: Envíe la variable de estado como prop */}
-          <HydrologyTable data={ }></HydrologyTable>
+          <HydrologyTable data={data}></HydrologyTable>
         
         </Grid>
         
--- src/components/HydrologyTable.tsx
+++ src/components/HydrologyTable.tsx
@@ -12,9 +12,9 @@ import DataHour from '../interface/DataHour';
 
 export default function HydrologyTable( { data } : { data: DataHour[] } ) {
 
-  let [rows, setRows] = useState(Array<DataHour>)
+  const [rows, setRows] = useState(Array<DataHour>)
 
-  let getRows = () => {
+  const getRows = () => {
     if (rows.length) {
       return (
         rows.map((row, idx) => (
@@ -28,6 +28,9 @@ export default function HydrologyTable( { data } : { data: DataHour[] } ) {
             <TableCell align="center">{row.Mazar}</TableCell>
             
             {/* PENDIENTE: Valores a renderizar en cada celda  */}
+            <TableCell align="center">{row.Molino}</TableCell>
+            <TableCell align="center">{row.Sopladora}</TableCell>
+           
             
             
           </TableRow>
@@ -53,8 +56,9 @@ export default function HydrologyTable( { data } : { data: DataHour[] } ) {
             <TableCell align='center'>Mazar</TableCell>
             
             {/* PENDIENTE: Cabeceras de las columnas  */}
+            <TableCell align='center'>Molino</TableCell>
+            <TableCell align='center'>Sopladora</TableCell>
             
-
           </TableRow>
         </TableHead>
         <TableBody>
